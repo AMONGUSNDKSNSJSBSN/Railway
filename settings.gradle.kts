039e3905db78159feef25163da52a56a002bc860
AMONGUSNDKSNSJSBSN <148880151+AMONGUSNDKSNSJSBSN@users.noreply.github.com>
2025-04-05 21:25:44 +0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ pluginManagement {
         mavenLocal()
         gradlePluginPortal()
         maven { url = uri("https://maven.neoforged.net/releases") }
-        maven { url = uri("https://maven.fabricmc.net/") }
         maven { url = uri("https://maven.architectury.dev/") }
         maven { url = uri("https://maven.quiltmc.org/repository/release") }
         maven {
@@ -15,9 +14,7 @@ pluginManagement {
 }
 
 include("common")
-include("fabric")
 include("forge")
-
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
